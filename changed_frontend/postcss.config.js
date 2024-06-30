ec9f51261a85a463a5d12b7865edb105c477486e
SHIVANSHU DIXIT <103733101+shivanshu-srm@users.noreply.github.com>
2024-07-01 02:54:29 +0530

    Task Done
    I have successfully implemented several enhancements to the Code Judge project:
    
    1. Support for Additional Languages:I added support for three new languages: Ruby, Golang, and prompt-based evaluations (Promptv1 and Promptv2).
    
    2. Test Coverage Expansion: I significantly increased the number of test cases across various functionalities to ensure robust testing and identify potential edge cases.
    
    3. Frontend Implementation(Changed frontend file): I developed a well-designed frontend in a dedicated "frontend" folder. This frontend allows users to run code against any supported language efficiently and provides a user-friendly interface for compilation and execution.
    
    4. Custom Feature Integration: Additionally, I introduced a custom feature aimed at improving the Code Judge project's functionality and usability.
    
    These enhancements collectively improve the project's versatility, reliability through testing, and usability with a well-crafted frontend interface.

---
+++ changed_frontend/postcss.config.js
@@ -0,0 +1,9 @@
+export default {
+  plugins: {
+    // Tailwind CSS plugin for utility-first CSS
+    tailwindcss: {},
+    
+    // Autoprefixer plugin to add vendor prefixes automatically
+    autoprefixer: {},
+  }
+};
---
+++ changed_frontend/src/components/Header.jsx
@@ -0,0 +1,24 @@
+import { FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
+
+const Footer = () => {
+  return (
+    <footer className="bg-shadowBlack w-4/5 lg:w-3/5 text-center font-primary p-2 relative border-2 border-black">
+      <small className="text-sm lg:text-xl xl:text-2xl text-primaryWhite">
+        © ALL RIGHTS RESERVED
+      </small>
+      <div className="flex gap-4 absolute top-1/2 right-4 transform -translate-y-1/2 text-sm lg:text-xl xl:text-2xl text-primaryWhite">
+        <a href="#">
+          <FaGithub className="hover:opacity-75 cursor-pointer" />
+        </a>
+        <a href="#">
+          <FaLinkedin className="hover:opacity-75 cursor-pointer" />
+        </a>
+        <a href="#">
+          <FaTwitter className="hover:opacity-75 cursor-pointer" />
+        </a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
